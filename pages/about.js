6ab2aa936e032636060bc09a2ad77c4b6a61487f
fujiwara02 <wuyin0023@gmail.com>
2023-01-03 03:22:56 +0900

    ok

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,24 @@
-import { Inter } from '@next/font/google'
 import styles from '../components/Main.module.css'
-import Links from '../components/Links'
+import Home_Links from '../components/Home_Links'
 import Headline from '../components/Headline'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-
-const inter = Inter({ subsets: ['latin'] })
+import Botton1 from '../components/Botton1'
 
 export default function Home() {
   return (
     <>   
-
+  
       <Header/>
+
+      <Botton1/>
 
       <Headline title = "About Pege" page="about"/>
       <main className={styles.main}>
         
         <Footer/>
 
-        <Links />
+        <Home_Links/>
         
       </main>
     </>
